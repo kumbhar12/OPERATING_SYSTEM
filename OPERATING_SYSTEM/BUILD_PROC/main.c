b39ea5d5e72da8b94f234e4c62495dc06858413c
kumbhar12 <anikumbhar810@gmail.com>
2021-12-08 08:11:52 +0530

    Build Process MakeFile

---
+++ OPERATING_SYSTEM/BUILD_PROC/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "math.h"
+
+
+int main(int argc,char const *argv[])
+{
+    int result=0;
+    result=add(10,20);
+    printf("addition =%d",result);
+    return 0 ;
+}
