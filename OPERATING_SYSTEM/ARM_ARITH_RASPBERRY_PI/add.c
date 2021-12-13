28f502716ed426fbfe628e9d13b90ff6a7d995fe
kumbhar12 <anikumbhar810@gmail.com>
2021-12-13 11:23:57 +0530

    RPi Assignment

---
+++ OPERATING_SYSTEM/ARM_ARITH_RASPBERRY_PI/add.c
@@ -0,0 +1,8 @@
+int add(int a, int b, int c)
+{
+    int sum =0;
+    sum = a + b + c;
+
+    return sum;
+
+}                        
---
+++ OPERATING_SYSTEM/ARM_ARITH_RASPBERRY_PI/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int result = 0;
+    result = add(10, 20, 30);
+    printf("Addition: %d\n", result);
+
+    result = sub(10, 20);
+    printf("subtraction: %d\n", result);
+    return 0;
+}
